9f7ecaa043ea67826e785b38bab0d58593aba7c5
chauvanhung <chauvanhung1999@gmail.com>
2020-07-25 16:30:08 +0700

    update 5

--- GameDHMT/GameDHMT/main.cpp
+++ GameDHMT/GameDHMT/main.cpp
@@ -1,10 +1,10 @@
-//Chau Van Hung 
+﻿//Chau Van Hung 
 //DoHoaMayTinh
 #include "Dependencies\glew\glew.h"
 #include "Dependencies\freeglut\freeglut.h"
 
-//const int screenWidth = 640;
-//const int screenHeight = 480;
+//const int screenWidth = 640;//ap dung cho bai 1 tới 12 lap 1 va bai 2 lap 2
+//const int screenHeight = 480;//ap dung cho bai 1 tới 12 lap 1 va bai 2 lap 2
 //const int screenWidth = 250;//bai 13
 //const int screenHeight = 250;//bai 13
 
@@ -865,6 +865,7 @@ int main(int argc, char** argv)
 }
 
 */
+//bai 14- lap 1 chua sua xong
 //#include <math.h>
 //
 //const double R = 150;
@@ -943,135 +944,496 @@ int main(int argc, char** argv)
 
 //bai 1 -lap 2
 
-#include <math.h>
-#define PI 3.14159265f
+//#include <math.h>
+//#define PI 3.14159265f
+//
+////Global variables
+//char title[] = "Bouncing Ball (2D)";
+//int windowWitdh = 640;
+//int windowHeight = 480;
+//int windowPosX = 50;
+//int windowPosY = 50;
+//
+//GLfloat ballRadius = 0.5f;
+//GLfloat ballX = 0.0f;
+//GLfloat ballY = 0.0f;
+//GLfloat ballXMax, ballXMin, ballYMax, ballYMin;
+//GLfloat xSpeed = 0.02f;
+//GLfloat ySpeed = 0.007f;
+//int refreshMillis = 30;
+//
+//GLdouble clipAreaXLeft, clipAreaXRight, clipAreaYBottom, clipAreaYTop;
+//
+//void InitGL()
+//{
+//	glClearColor(0.0, 0.0, 0.0, 1.0);
+//
+//}
+//
+//void Display()
+//{
+//	glClear(GL_COLOR_BUFFER_BIT);
+//	glMatrixMode(GL_MODELVIEW);
+//	glLoadIdentity();
+//
+//	glTranslatef(ballX, ballY, 0.0f);
+//
+//	glBegin(GL_TRIANGLE_FAN);
+//	glColor3f(0.0f, 0.0f, 1.0f);
+//	glVertex2f(0.0f, 0.0f);
+//	int numSegments = 100;
+//	GLfloat angle;
+//	for (int i = 0; i <= numSegments; i++)
+//	{
+//
+//		angle = i * 2.0f * PI / numSegments;
+//		glVertex2f(cos(angle) * ballRadius, sin(angle) * ballRadius);
+//	}
+//		glEnd();
+//
+//		glutSwapBuffers();
+//	
+//	ballX += xSpeed;
+//	ballY += ySpeed;
+//
+//	if (ballX > ballXMax)
+//	{
+//		ballX = ballXMax;
+//		xSpeed = -xSpeed;
+//	}
+//
+//	else if (ballX < ballXMin)
+//	{
+//		ballX = ballXMin;
+//		xSpeed = -xSpeed;
+//	}
+//
+//	if (ballY > ballYMax)
+//	{
+//		ballY = ballYMax;
+//		ySpeed = -ySpeed;
+//	}
+//	else if (ballY < ballYMin)
+//	{
+//		ballY = ballYMin;
+//		ySpeed = -ySpeed;
+//	}
+//}
+//
+//void Reshape(GLsizei width, GLsizei height)
+//{
+//	if (height == 0) height = 1;
+//	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+//
+//	glViewport(0, 0, width, height);
+//
+//	glMatrixMode(GL_PROJECTION);
+//	glLoadIdentity();
+//
+//	if (width >= height)
+//	{
+//		clipAreaXLeft = -1.0 * aspect;
+//		clipAreaXRight = 1.0 * aspect;
+//		clipAreaYBottom = -1.0;
+//		clipAreaYTop = 1.0;
+//	}
+//
+//	else
+//	{
+//		clipAreaXLeft = -1.0;
+//		clipAreaXRight = 1.0;
+//		clipAreaYBottom = -1.0 / aspect;
+//		clipAreaYTop = 1.0 / aspect;
+//	}
+//	gluOrtho2D(clipAreaXLeft, clipAreaXRight, clipAreaYBottom, clipAreaYTop);
+//	ballXMin = clipAreaXLeft + ballRadius;
+//	ballXMax = clipAreaXRight - ballRadius;
+//	ballYMin = clipAreaYBottom + ballRadius;
+//	ballYMax = clipAreaYTop - ballRadius;
+//
+//}
+//
+//void Timer(int value)
+//{
+//	glutPostRedisplay();
+//	glutTimerFunc(refreshMillis, Timer, 0);
+//
+//}
+//
+//int main(int argc, char** argv)
+//{
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_DOUBLE);
+//	glutInitWindowSize(windowWitdh, windowHeight);
+//
+//	glutInitWindowPosition(windowPosX, windowPosY);
+//
+//	glutCreateWindow(title);
+//	glutDisplayFunc(Display);
+//	glutReshapeFunc(Reshape);
+//	glutTimerFunc(0, Timer, 0);
+//	InitGL();
+//	glutMainLoop();
+//	return 0;
+//}
 
-//Global variables
-char title[] = "Bouncing Ball (2D)";
-int windowWitdh = 640;
-int windowHeight = 480;
-int windowPosX = 50;
-int windowPosY = 50;
+//bai 2 - lap 2
+//
+//void Init(void)
+//{
+//	glClearColor(0.0, 0.0, 0.0, 0.0);
+//	glShadeModel(GL_SMOOTH);
+//}
+//
+//void Display()
+//{
+//	glClear(GL_COLOR_BUFFER_BIT);
+//	glColor3f(1.0, 1.0, 1.0);
+//
+//	glutSolidTeapot(0.5);
+//
+//	glFlush();
+//}
+//
+//void Reshape(int w, int h)
+//{
+//	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+//	glMatrixMode(GL_MODELVIEW);
+//	glLoadIdentity();
+//	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+//}
+//int main(int argc, char** argv)
+//{
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+//	glutInitWindowSize(screenWidth,screenHeight);
+//
+//	glutInitWindowPosition(100,100);
+//
+//	glutCreateWindow(argv[0]);
+//	Init();
+//	glutDisplayFunc(Display);
+//	glutReshapeFunc(Reshape);
+//	
+//	glutMainLoop();
+//	return 0;
+//}
 
-GLfloat ballRadius = 0.5f;
-GLfloat ballX = 0.0f;
-GLfloat ballY = 0.0f;
-GLfloat ballXMax, ballXMin, ballYMax, ballYMin;
-GLfloat xSpeed = 0.02f;
-GLfloat ySpeed = 0.007f;
-int refreshMillis = 30;
+//bai 3 -lap 2
+#include<math.h>
+#include<iostream>
 
-GLdouble clipAreaXLeft, clipAreaXRight, clipAreaYBottom, clipAreaYTop;
+void InitGlut(int argc, char** argv);
+void DisplayFunc(void);
+void IdleFunc(void);
+void ReshapeFunc(int width, int height);
+void MouseFunc(int button, int state, int x, int y);
+void KeyBoardFunc(unsigned char key, int x, int y);
+void SpecialFunc(int key, int x, int y);
+void MouseMotionFunc(int x, int y);
+//-----------------------------------------------------------//
 
-void InitGL()
+//other[openGL] functions
+void CountFrames(void);
+void RenderBitmapString(float x, float y, float z, void* font, char* string);
+//----------------------------------------------------------------------------//
+
+bool bUserPredefinedCamera = true;
+
+bool bFullScreen = false;
+int nWindowID;
+
+//camera attributes
+float ViewerPosition[3] = { 0.0,0.0,-50.0 };
+float ViewerDirection[3] = { 0.0,0.0,0.0 };
+float ViewerUp[3] = { 0.0,1.0,0.0 };
+
+//roatation values for the navigation
+
+float NavigationRotation[3] = { 0.0,0.0,0.0 };
+//--------------------------------------------------//
+
+//framecounter
+char pixelstring[30];
+int cframe = 0;
+int time = 0;
+int timebase = 0;
+
+//parameters for the navigation
+
+//position of the mouse when pressed
+
+int MousePressedX = 0, MousePressedY = 0;
+float LastXOffset = 0.0, LastYOffset = 0.0, LastZOffset = 0.0;
+
+//mouse button states
+int LeftMouseButtonActive = 0,
+MiddleMouseButtonActive = 0,
+RightMouseButtonActive = 0;
+
+//modifier state
+
+int ShiftActive = 0, AltActive = 0, CtrlActive = 0;
+
+//---------------------------------------------------------------//
+bool Init = false;
+//----------------------------------------------------------------//
+
+void DisplayFunc(void)
 {
-	glClearColor(0.0, 0.0, 0.0, 1.0);
+	//Clear the buffers
 
-}
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-void Display()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(50.0, 1.33, 1.0, 100000.0);
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	glTranslatef(ballX, ballY, 0.0f);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
 
-	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex2f(0.0f, 0.0f);
-	int numSegments = 100;
-	GLfloat angle;
-	for (int i = 0; i <= numSegments; i++)
-	{
+	GLfloat lightpos[4] = { 5.0,15.0,10.0,1.0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
 
-		angle = i * 2.0f * PI / numSegments;
-		glVertex2f(cos(angle) * ballRadius, sin(angle) * ballRadius);
-	}
-		glEnd();
+	glTranslatef(ViewerPosition[0], ViewerPosition [1],ViewerPosition[2] );
 
-		glutSwapBuffers();
-	
-	ballX += xSpeed;
-	ballY += ySpeed;
+	//add navigation rotation
 
-	if (ballX > ballXMax)
-	{
-		ballX = ballXMax;
-		xSpeed = -xSpeed;
-	}
+	glRotatef(NavigationRotation[0], 1.0f, 0.0f, 0.0f);
+	glRotatef(NavigationRotation[1], 0.0f, 1.0f, 0.0f);
 
-	else if (ballX < ballXMin)
-	{
-		ballX = ballXMin;
-		xSpeed = -xSpeed;
-	}
+	glutSolidTeapot(10.0);
 
-	if (ballY > ballYMax)
-	{
-		ballY = ballYMax;
-		ySpeed = -ySpeed;
-	}
-	else if (ballY < ballYMin)
-	{
-		ballY = ballYMin;
-		ySpeed = -ySpeed;
-	}
+	CountFrames();
+
+	glutSwapBuffers();
 }
 
-void Reshape(GLsizei width, GLsizei height)
-{
-	if (height == 0) height = 1;
-	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+//---------------------------------------------------------------------------------------//
 
+void InitGlut(int argc, char** argv)
+{
+	//
+	glutInit(&argc, argv);
+	glutInitWindowSize(640, 480);
+	glutInitWindowPosition(100, 100);
+	glutInitDisplayMode(GLUT_RGBA | GL_DOUBLE | GLUT_DEPTH);
+	nWindowID = glutCreateWindow("simpleGLUT");
+
+	//Register callbacks
+	glutDisplayFunc(DisplayFunc);
+	glutReshapeFunc(ReshapeFunc);
+	glutKeyboardFunc(KeyBoardFunc);
+	glutSpecialFunc(SpecialFunc);
+	glutMouseFunc(MouseFunc);
+	glutMotionFunc(MouseMotionFunc);
+	glutIdleFunc(IdleFunc);
+}
+
+//-------------------------------------------------------------------------------//
+
+void IdleFunc(void)
+{
+	glutPostRedisplay();
+}
+
+void ReshapeFunc(int width, int height)
+{
 	glViewport(0, 0, width, height);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
+	gluPerspective(75.0, 1.33, 1.0, 1000.0);
 
-	if (width >= height)
+	glMatrixMode(GL_MODELVIEW);
+}
+
+//mouse callback
+
+void MouseFunc(int button, int state, int x, int y)
+{
+	//get the modifiers
+	switch (glutGetModifiers())
 	{
-		clipAreaXLeft = -1.0 * aspect;
-		clipAreaXRight = 1.0 * aspect;
-		clipAreaYBottom = -1.0;
-		clipAreaYTop = 1.0;
+	case GLUT_ACTIVE_SHIFT:
+			ShiftActive = 1;
+			break;
+	case GLUT_ACTIVE_ALT:
+		AltActive = 1;
+		break;
+	case GLUT_ACTIVE_CTRL:
+		CtrlActive = 1;
+		break;
+	default:
+		ShiftActive = 0;
+		AltActive = 0;
+		CtrlActive = 0;
+		break;
 	}
 
+	if (button == GLUT_LEFT_BUTTON)
+		if (state == GLUT_DOWN)
+		{
+			LeftMouseButtonActive += 1;
+		}
+		else
+			LeftMouseButtonActive -= 1;
 	else
+		if (button == GLUT_MIDDLE_BUTTON)
+			if (state = GLUT_DOWN)
+			{
+				RightMouseButtonActive += 1;
+				LastZOffset = 0.0;
+			}
+			else
+				RightMouseButtonActive -= 1;
+
+			//if(altactive){
+				MousePressedX = x;
+				MousePressedY = y;
+			//}
+	
+}
+
+void MouseMotionFunc(int x, int y)
+{
+	float xOffset = 0.0, yOffset = 0.0, zOffset = 0.0;
+	if (LeftMouseButtonActive)
 	{
-		clipAreaXLeft = -1.0;
-		clipAreaXRight = 1.0;
-		clipAreaYBottom = -1.0 / aspect;
-		clipAreaYTop = 1.0 / aspect;
+		NavigationRotation[0] += ((MousePressedY - y) * 180.0f) / 200.0f;
+		NavigationRotation[1] += ((MousePressedX - x) * 180.0f) / 200.0f;
+
+		MousePressedY = y;
+		MousePressedX = x;
+
 	}
-	gluOrtho2D(clipAreaXLeft, clipAreaXRight, clipAreaYBottom, clipAreaYTop);
-	ballXMin = clipAreaXLeft + ballRadius;
-	ballXMax = clipAreaXRight - ballRadius;
-	ballYMin = clipAreaYBottom + ballRadius;
-	ballYMax = clipAreaYTop - ballRadius;
+	else if (MiddleMouseButtonActive)
+	{
+		xOffset = (MousePressedX + x);
+		if (!LastXOffset == 0.0)
+		{
+			ViewerPosition[0] -= (xOffset - LastXOffset) / 8.0;
+			ViewerDirection[0] -= (xOffset - LastXOffset) / 8.0;
+		}
+		LastXOffset = xOffset;
+
+		yOffset = (MousePressedY + y);
+		if (!LastYOffset == 0.0)
+		{
+			ViewerPosition[1] += (yOffset - LastYOffset) / 8.0;
+			ViewerDirection[1] += (yOffset - LastYOffset) / 8.0;
+		}
+		LastZOffset = zOffset;
+	}
+
+	else if (RightMouseButtonActive)
+	{
+		zOffset = (MousePressedX + x);
+		if (!LastZOffset == 0.0)
+		{
+			ViewerPosition[2] -= (zOffset - LastZOffset) / 8.0;
+			ViewerDirection[2] -= (zOffset - LastZOffset) / 8.0;
+
+
+		}
+		LastZOffset = zOffset;
+	}
+}
+void KeyBoardFunc(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+#ifdef Win32
+
+		//exit on escape
+	case '\003':
+		exit(0);
+		break;
+#endif
+		//------------------------------------------------------//
+
+		//switch to fullscreen
+	case 'f':
+
+		bFullScreen = !bFullScreen;
+		if (bFullScreen)
+			glutFullScreen();
+		else
+		{
+			glutSetWindow(nWindowID);
+			glutPositionWindow(100, 100);
+			glutReshapeWindow(640, 480);
+		}
+		break;
+		//--------------------------
+	}
+}
+	//-----------------------
+
+void SpecialFunc(int key, int x, int y)
+{
+}
+
+//-----------------------------------//
+
+void CountFrames(void)
+{
+	time = glutGet(GLUT_ELAPSED_TIME);
+	cframe++;
+	if(time - timebase > 50)
+	{	
+		printf(pixelstring, "fps: %4.2f", cframe * 1000.0 / timebase);
+		timebase = time;
+		cframe = 0;
+		//Draw status text and uni-logo:
+	}
+	 
+	glPushMatrix();
+	glLoadIdentity();
+	glDisable(GL_LIGHTING);
+	glColor4f(1.0, 1.0, 1.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0, 200, 0, 200);
+	glMatrixMode(GL_MODELVIEW);
+
+	//render string
+	RenderBitmapString(5, 5, 0.0, GLUT_BITMAP_HELVETICA_10, pixelstring);
+
+	glPopMatrix();
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
 
 }
 
-void Timer(int value)
-{
-	glutPostRedisplay();
-	glutTimerFunc(refreshMillis, Timer, 0);
+//------------------------------------------------//
 
+void RenderBitmapString(float x, float y, float z, void* font, char* string)
+{
+	char* c;
+	glRasterPos3f(x, y, z);
+	for (c = string; *c != '\0'; c++)
+	{
+		glutBitmapCharacter(font, *c);
+	}
 }
 
-int main(int argc, char** argv)
+//--------------------------------------
+
+void main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE);
-	glutInitWindowSize(windowWitdh, windowHeight);
+	printf("SipmpleGLUT\n\tGordon Wetzstein [gordon.wetzstein@medien.uni-weimar.de]\n\n");
+	printf("key:\n\tf\t- togglr fullscreen\n\tesc\t- exit\n\n");
+	printf("mouse:\n\tleft   button\t-   rotation\n\tmiddle   button\t- pannig\n\tright button\t- zoom in and out\n");
 
-	glutInitWindowPosition(windowPosX, windowPosY);
-
-	glutCreateWindow(title);
-	glutDisplayFunc(Display);
-	glutReshapeFunc(Reshape);
-	glutTimerFunc(0, Timer, 0);
-	InitGL();
+	InitGlut(argc, argv);
 	glutMainLoop();
-	return 0;
 }
